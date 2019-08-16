f09122948c51c4f8cfb538cc200420a20e96cec6
skaiste <25135643+skaisteauguste@users.noreply.github.com>
2019-08-16 12:34:15 +0200

    week6. Petition: fixed canvas to be able to paint signature on it.

--- public/script.js
+++ public/script.js
@@ -1,28 +1,8 @@
-console.log("sanity check");
-
-var box = document.getElementById("signInput");
-window.addEventListener("mousemove", move, true);
-
-/////////
+// console.log("sanity check");
 
 const paintCanvas = document.querySelector(".js-paint");
 const context = paintCanvas.getContext("2d");
 context.lineCap = "round";
-
-const colorPicker = document.querySelector(".js-color-picker");
-
-colorPicker.addEventListener("change", event => {
-    context.strokeStyle = event.target.value;
-});
-
-const lineWidthRange = document.querySelector(".js-line-range");
-const lineWidthLabel = document.querySelector(".js-range-value");
-
-lineWidthRange.addEventListener("input", event => {
-    const width = event.target.value;
-    lineWidthLabel.innerHTML = width;
-    context.lineWidth = width;
-});
 
 let x = 0,
     y = 0;
