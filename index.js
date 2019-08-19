99166e5a75e44627c3e75f145ee5eb6a60002557
skaiste <25135643+skaisteauguste@users.noreply.github.com>
2019-08-19 15:46:02 +0200

    Added simple /register and /login routes

--- index.js
+++ index.js
@@ -40,31 +40,60 @@ app.use(
     })
 );
 
-app.get("/", (req, res) => {
-    //lets look at the hash function.
-    // would probably look like req.body.password
-    hash("12345")
-        .then(hash => {
-            console.log("hash: ", hash);
-            //lets look at compare
-            compare("12345", hash)
-                .then(match => {
-                    console.log("did my pasword match?");
-                    console.log(match);
-                })
-                .catch(e => console.log(e));
-        })
-        .catch(e => console.log(e));
-    //res.redirect('/petition');
-    // db.testFunction();
+// app.get("/", (req, res) => {
+//     //lets look at the hash function.
+//     // would probably look like req.body.password
+//     hash("12345")
+//         .then(hash => {
+//             console.log("hash: ", hash);
+//             //lets look at compare
+//             compare("12345", hash)
+//                 .then(match => {
+//                     console.log("did my pasword match?");
+//                     console.log(match);
+//                 })
+//                 .catch(e => console.log(e));
+//         })
+//         .catch(e => console.log(e));
+//     //res.redirect('/petition');
+//     // db.testFunction();
+// });
+
+app.get("/register", (req, res) => {
+    res.render("register");
 });
+
+app.post("/register", (req, res) => {
+    db.addUser(
+        req.body.firstname,
+        req.body.lastname,
+        req.body.email,
+        req.body.password
+    )
+        .then(result => {
+            console.log("result :", result);
+            res.render("petition");
+        })
+        .catch(err => {
+            console.log("ERROR :", err);
+            res.render("register", {
+                error: true
+            });
+        });
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.post("/login", (req, res) => {});
 
 app.get("/petition", (req, res) => {
     res.render("petition");
 });
 
 app.post("/petition", (req, res) => {
-    db.addSignature(req.body.firstname, req.body.lastname, req.body.signature)
+    db.addSignature(req.body.signature, req.session.user_id)
         .then(id => {
             console.log(id);
             req.session.signatureId = id;
@@ -79,8 +108,8 @@ app.post("/petition", (req, res) => {
 });
 
 app.get("/thanks", (req, res) => {
-    if (req.session.signatureId) {
-        db.getSignature(req.session.signatureId).then(result => {
+    if (req.session.user_id) {
+        db.getSignature(req.session.user_id).then(result => {
             console.log("result :", result);
             res.render("thanks", {
                 signature: result
