3bddd2c3be251c25eb356a69de11fbe8e353c957
skaiste <25135643+skaisteauguste@users.noreply.github.com>
2019-08-20 12:06:04 +0200

    week7. Petition: added ression cookies on userId.

--- index.js
+++ index.js
@@ -56,7 +56,12 @@ app.post("/register", (req, res) => {
             );
         })
         .then(result => {
-            console.log("result :", result);
+            console.log("RESULT :", result);
+            // in reult i will have id of user, that data base has line returning id.
+            // If it does it will be in result could be a number, or inside array or object
+            //req.session.userId = result(if it was a numberif not deal with it)
+            req.session.userId = result.rows[0].id;
+            console.log("result.rows[0].id :", result.rows[0].id);
             res.render("petition");
         })
         .catch(err => {
@@ -72,27 +77,32 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-    db.getPassword(req.body.emailaddress)
-        .then(hashedPsw => {
-            return compare(req.body.pwd, hashedPsw);
-        })
-        .then(match => {
-            console.log("did my pasword match?");
-            console.log(match);
-            if (match) {
-                res.redirect("petition");
-            } else {
+    db.getPassword(req.body.emailaddress).then(hashedPsw => {
+        console.log("hashedPsw :", hashedPsw);
+        compare(req.body.pwd, hashedPsw.password)
+            .then(match => {
+                console.log("did my pasword match?");
+                console.log(match);
+                if (match) {
+                    req.session.userId = hashedPsw.id;
+                    // if ()
+                    //if the user has a row in the signatures
+                    // table first take that id and put into req.session.signaturesId
+                    // and then I redirect to thank you. If they don't I redirect to pettition
+                    res.redirect("/petition");
+                } else {
+                    res.render("login", {
+                        error: true
+                    });
+                }
+            })
+            .catch(err => {
+                console.log("ERROR :", err);
                 res.render("login", {
                     error: true
                 });
-            }
-        })
-        .catch(err => {
-            console.log("ERROR :", err);
-            res.render("login", {
-                error: true
             });
-        });
+    });
 });
 
 app.get("/petition", (req, res) => {
@@ -115,6 +125,7 @@ app.post("/petition", (req, res) => {
 });
 
 app.get("/thanks", (req, res) => {
+    console.log("req.session.thanks: ", req.session);
     if (req.session.signatureId) {
         db.getSignature(req.session.signatureId).then(result => {
             console.log("result :", result);
@@ -128,6 +139,7 @@ app.get("/thanks", (req, res) => {
 });
 
 app.get("/signers", (req, res) => {
+    console.log("req.session.signers: ", req.session);
     if (req.session.signatureId) {
         db.getInfo()
             .then(result => {
