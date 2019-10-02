94923d89a0674ecd9df433f209e74fcf30938742
skaiste <25135643+skaisteauguste@users.noreply.github.com>
2019-10-03 00:02:25 +0200

    Did minor change

--- petition.sql
+++ petition.sql
@@ -3,14 +3,14 @@ DROP TABLE IF EXISTS signatures CASCADE;
 CREATE TABLE signatures (
     id SERIAL PRIMARY KEY,
     signature TEXT NOT NULL CHECK (signature != ''),
-    user_id INTEGER NOT NULL,  --REFERENCES users(id)
+    user_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
  );
 
 DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY, -- references users(id)
     firstname VARCHAR(255) NOT NULL CHECK (firstname != ''),
     lastname VARCHAR(255) NOT NULL CHECK (lastname != ''),
     email VARCHAR(255) NOT NULL UNIQUE,
